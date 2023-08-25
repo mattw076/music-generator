23b47d51abf5c5cb0934295852a80e6ce2ac9800
Matt Wyatt <MDW@intranet.macro4.com>
2023-08-25 16:52:16 +0100

    Form changed to use React state (inputs are controlled components)

--- src/components/GenrePickerForm/GenrePickerForm.jsx
+++ src/components/GenrePickerForm/GenrePickerForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState } from 'react'
 import styles from './GenrePickerForm.module.scss';
 
 import spotifyData from "./SpotifyData.js";
@@ -6,6 +6,22 @@ import spotifyData from "./SpotifyData.js";
 const GenrePickerForm = (props) => {
 
     const { setSong } = props;
+
+    const [formData, setFormData] = useState({
+        genre: "",
+        energy: "" 
+    });
+    
+    console.log(formData);
+
+    const handleChange = (e) => {
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [e.target.name]: e.target.value
+            }
+        });
+    }
 
     const handleClick = async (e) => {
         e.preventDefault();
@@ -27,11 +43,14 @@ const GenrePickerForm = (props) => {
 
     return (
         <form className={styles.form} >
-            <input className={styles.input} type="text" placeholder="Genre" />
-            <input className={styles.input} type="text" placeholder="Energy" />
+            <input className={styles.input} type="text" placeholder="Genre" onChange={handleChange} name="genre" value={formData.genre}/>
+            <input className={styles.input} type="number" placeholder="Energy" min="0" max="100" onChange={handleChange} name="energy" value={formData.energy}/>
             <button className={styles.button} onClick={handleClick}>Get song</button>
         </form>
     )
 }
+
+// NOTE: we specifiy the value of the inputs as being based on the value of their respective state (make sure to define a name attribute), rather than leaving it to the built-in HTML inout "state".
+// These are called "controlled components": https://legacy.reactjs.org/docs/forms.html
 
 export default GenrePickerForm
