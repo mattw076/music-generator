81567299b67ad652a033763188e561b83fbfea0f
Matt Wyatt <MDW@intranet.macro4.com>
2023-10-20 13:01:10 +0100

    Toggle label passed as props

--- src/components/GenrePickerHistory/GenrePickerHistory.jsx
+++ src/components/GenrePickerHistory/GenrePickerHistory.jsx
@@ -8,26 +8,26 @@ const GenrePickerHistory = (props) => {
 
     const { history, handleClickStar } = props;
 
-    const [favouritesToggled, setFavouritesToggled ] = useState(false);
+    const [favouritesToggled, setFavouritesToggled] = useState(false);
 
     const handleChangeFavouritesToggled = () => {
         setFavouritesToggled(!favouritesToggled);
     }
 
-    const historyItems = history.slice(1).map((song, i) => <GenrePickerHistoryItem key={i+1} song={song} history={history} id={i+1} handleClickStar={handleClickStar}/>);
+    const historyItems = history.slice(1).map((song, i) => <GenrePickerHistoryItem key={i + 1} song={song} history={history} id={i + 1} handleClickStar={handleClickStar} />);
     const historyItemsFavourited = historyItems.filter(item => item.props.song.favourite === true);
     // Q: is having 2 different arrays of items the right way to do it? Seems inefficient
 
     return (
         <div className={styles.history}>
             <h3 className={styles.heading}>History</h3>
-            <Toggle isToggled={favouritesToggled} handleChange={handleChangeFavouritesToggled}/>
+            <Toggle isToggled={favouritesToggled} handleChange={handleChangeFavouritesToggled} labelText={"Show only favourites"} />
             {favouritesToggled ? historyItemsFavourited : historyItems}
             {/* Two below together are equivalent to above:
             {favouritesToggled && historyItemsFavourited}
-            {!favouritesToggled && historyItems} */}
-            { !favouritesToggled && historyItems.length === 0 && <p className={styles.placeholder}>No song history to display yet.</p> }
-            { favouritesToggled && historyItemsFavourited.length === 0 && <p className={styles.placeholder}>No favourite songs to display yet.</p> }
+        {!favouritesToggled && historyItems} */}
+            {!favouritesToggled && historyItems.length === 0 && <p className={styles.placeholder}>No song history to display yet.</p>}
+            {favouritesToggled && historyItemsFavourited.length === 0 && <p className={styles.placeholder}>No favourite songs to display yet.</p>}
         </div>
     )
 }
--- src/components/Toggle/Toggle.jsx
+++ src/components/Toggle/Toggle.jsx
@@ -4,14 +4,14 @@ import styles from './Toggle.module.scss';
 
 const Toggle = (props) => {
 
-    const {isToggled, handleChange } = props;
+    const {isToggled, handleChange, labelText } = props;
 
     // TODO: label should be passed in as props to be reusable
     // NOTE: each Toggle will need a unique id
 
     return (
         <label className={styles.switch}>
-            <span className={styles.text}>Show only favourites</span>
+            <span className={styles.text}>{labelText}</span>
             <input type="checkbox" checked={isToggled} onChange={handleChange}/>
             <span className={`${styles.slider} ${styles.round}`}></span>
         </label>
